6ebb7b59e541a8c5b75a1f032848e330b926625b
María Rodríguez <lu.rzapien.92@gmail.com>
2023-05-25 15:41:32 -0600

    reto reto 2  terminado

--- reto_02/apiRequest.js
+++ reto_02/apiRequest.js
@@ -1,29 +1,48 @@
-const http = require('http');
+const http = require("http");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const word = "example";
 
-const word = 'example';
+app.use(express.static(__dirname));
 
-const options = {
-  hostname: 'localhost',
-  port: 80,
-  path: `/api/v2/entries/en/${word}`,
-  method: 'GET'
-};
+function getEntries(response) {
+  const options = {
+    hostname: "api.dictionaryapi.dev",
+    port: 80,
+    path: `/api/v2/entries/en/${word}`,
+    method: "GET",
+  };
 
-const req = http.request(options, (res) => {
-  let data = '';
+  const req = http.request(options, (res) => {
+    let data = "";
 
-  res.on('data', (chunk) => {
-    data += chunk;
+    res.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    res.on("end", () => {
+      const jsonResponse = JSON.parse(data);
+      response.status(200).send({ message: "Ok", data: jsonResponse });
+    });
   });
 
-  res.on('end', () => {
-    const jsonResponse = JSON.parse(data);
-    console.log(jsonResponse);
+  req.on("error", (error) => {
+    console.error(`Error making API request: ${error}`);
+    response.status(500).send({ message: "Server error" })
   });
+
+  req.end();
+}
+
+app.listen(port, () => {
+  console.log(`Reto_02 Server is running in ${port}`);
 });
 
-req.on('error', (error) => {
-  console.error(`Error making API request: ${error}`);
+app.get("/", (req, res) => {
+  res.render(__dirname + "/index.html");
 });
 
-req.end();
+app.get("/entries", async (req, res) => {
+  getEntries(res);
+});
